a51e22bf5f1aa6d8f7b5183db796892dc0b5ccad
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-17 18:56:39 +0300

    task04 logic has been added without loging and checking for NullRefernces

--- Task04Logic/BinaryBookListStorage.cs
+++ Task04Logic/BinaryBookListStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,56 @@ namespace Task04Logic
 {
     public class BinaryBookListStorage : IBookListStorage
     {
+        private readonly string _fileName;
+
         public BinaryBookListStorage(string fileName)
         {
-
+            _fileName = fileName;
         }
-
+        /// <summary>
+        /// loads books from file
+        /// </summary>
+        /// <returns>List of books</returns>
         public List<Book> LoadBooks()
         {
             List<Book> books = new List<Book>();
+            FileStream fs = File.OpenRead(_fileName);
+            var r = new BinaryReader(fs);
+
+            while (r.PeekChar() > -1)
+            {
+                string author = r.ReadString();
+                string title = r.ReadString();
+                int pages = r.ReadInt32();
+                int published = r.ReadInt32();
+
+                books.Add(new Book(author, title, pages, published));
+            }
+
+            fs.Close();
             return books;
         }
 
+        /// <summary>
+        /// saves list of books in file
+        /// </summary>
+        /// <param name="books">list of books</param>
         public void SaveBooks(IEnumerable<Book> books)
         {
+            FileStream fs = File.OpenWrite(_fileName);
+            BinaryWriter w = new BinaryWriter(fs);
 
+            foreach (Book b in books)
+            {
+                w.Write(b.Author);
+                w.Write(b.Title);
+                w.Write(b.Pages);
+                w.Write(b.Published);
+            }
+
+            w.Flush();
+            
+            fs.Close();
         }
     }
 }
---
+++ Task04UI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task04Logic;
+
+namespace Task04UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("q", "w", 6, 2);
+            Book book2 = new Book("q", "e", 3, 2);
+            Book book3 = new Book("q", "r", 1, 2);
+            Book book4 = new Book("q", "t", 4, 2);
+
+            string fileName = "source.txt";
+            
+            BookListStorageCreator creator = new BinaryBookListStorageCreator();
+            BookListService service = new BookListService(creator, fileName);
+
+            service.AddBook(book1);
+            service.AddBook(book2);
+            service.AddBook(book3);
+            service.AddBook(book4);
+
+            service.RemoveBook(book2);
+
+            Console.WriteLine(service.FindBookByTag(book3.Author, book3.Title, 
+                book3.Pages, book3.Published).ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
