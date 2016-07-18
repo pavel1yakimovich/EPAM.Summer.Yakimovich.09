83d2790528c0ed25436029ac3f817c823bacc57a
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-18 18:30:35 +0300

    task03 added

---
+++ Task03ConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(args[0]);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            char[] separator = {' ', ',', '.', '!', '@', '"', ')', '(', '?', ':', ';', '-', '_', '\''};
+            string[] result = {};
+            string text = "";
+
+            while (!reader.EndOfStream)
+            {
+                text += reader.ReadLine();
+            } 
+
+            result = text.Split(separator);
+
+            foreach (string word in result)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word]++;
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in dictionary)
+            {
+                Console.WriteLine($"Word: {pair.Key} was mentioned {pair.Value} time(s) in the text");
+            }
+
+            Console.ReadKey();
+            reader.Close();
+        }
+    }
+}
